612be9466d36e1f4524283a5a4d6d1b0e00b6486
JoseFelix <jfelixnunes2k15@gmail.com>
2025-06-04 22:52:21 -0300

    add archives of project filter FIR

--- ProjetoFIR_2/plotspec.m
+++ ProjetoFIR_2/plotspec.m
@@ -5,12 +5,12 @@
 function plotspec(x,Ts)
 
 N=length(x);                               % Comprimento do sinal x
-t=Ts*(1:N);                                % vetor de tempo 
+t=Ts*(1:N);                                % vetor de tempo
 ssf=(-N/2:N/2-1)/(Ts*N);                   % vetor de frequ�ncia
 fx=fft(x(1:N));                            % calcula a DFT/FFT
 fxs=fftshift(fx);                          % desloca para o centro do espectro
 subplot(2,1,1), plot(t,x)                  % apresenta o gr�fico da forma de onda
 xlabel('segundos'); ylabel('amplitude')     % rotula os eixos
 subplot(2,1,2), plot(ssf,abs(fxs))         % apresenta o gr�fico do espectro de magnitudes
-xlabel('frequ�ncia'); ylabel('magnitude')   % rotula os eixos
+xlabel('frequência'); ylabel('magnitude')   % rotula os eixos
 
