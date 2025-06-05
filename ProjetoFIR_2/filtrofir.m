7d9c817b6a125fbeda67019ce88fe70dab5c28f3
JoseFelix <jfelixnunes2k15@gmail.com>
2025-06-05 15:20:58 -0300

    correcao no filtro

--- ProjetoFIR_2/filtrofir.m
+++ ProjetoFIR_2/filtrofir.m
@@ -8,21 +8,24 @@ M = 156;       % Número de coeficientes (ordem do filtro + 1)
 w1 = 2*pi*fc/Fs;  % Frequência normalizada inferior
 w2 = 2*pi*fs/Fs;  % Frequência normalizada superior
 
-% Vetor de índices simétrico
-n = [0:M/2];       % Para M=156 (simétrico em torno de zero)
+wc = (w1 + w2)/2
 
+% Vetor de índices simétrico
+n = [0:M+1];       % Para M=156 (simétrico em torno de zero)
+
+N = n - (M/2)
 % Cálculo da resposta ideal (filtro passa-faixa)
-hd = (sin(w2*n) - sin(w1*n)) ./ (pi*n);
-hd(n == 0) = 0 ;  % Tratamento para n=0
+hd = (sin(w2*N) - sin(w1*N)) ./ (pi*N);
+hd(n == (M/2)) = wc/pi ;  % Tratamento para n=0
 
 % Janela de Hanning
 wHann = 0.5 - 0.5*cos(2*pi/(M));
 
 % Resposta ao impulso final
-h = hd .* wHann';
+hc = hd .* wHann';
 
 % Plotando o gráfico;
 Ts = 1/Fs;
-plotspec(h,Ts);
+plotspec(hc,Ts);
 
 
